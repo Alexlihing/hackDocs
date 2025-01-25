d43b23c513990445331d1ff01c9090d92f15ca10
aryangoel <aryangoel574@gmail.com>
2025-01-25 17:18:15 -0500

    added

---
+++ backend/routes/auth.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const router = express.Router();
+const { User } = require("../models/user");
+
+router.post("/user", async (req, res) => {
+  //input validation with joi.
+
+  const userDetails = req.body.userDetails;
+
+  const userEmail = userDetails.email;
+
+  const user = await User.findOne({ email: userEmail });
+
+  if (user) {
+    res.status(200).send(user);
+  } else {
+    res.status(400).send("user not found");
+  }
+});
