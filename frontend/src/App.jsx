eadef5f3b61b3dfb4c1864d91a343df043531328
aryangoel <aryangoel574@gmail.com>
2025-01-25 17:31:52 -0500

    added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,8 @@
-import { Routes, Route } from 'react-router-dom';
-import NavBar from './component/NavBar/NavBar';
-import HomePage from './component/HomePage/HomePage';
-import LoginPage from './component/LogInPage/LoginPage';
+import { Routes, Route } from "react-router-dom";
+import NavBar from "./component/NavBar/NavBar";
+import HomePage from "./component/HomePage/HomePage";
+import LoginPage from "./component/LogInPage/LoginPage";
+import "index.css";
 
 function App() {
   return (
