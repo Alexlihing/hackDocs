e45158a92a3ba1ada89b09fbef4947d797ac6564
AlexLi9132 <alexli@purdue.edu>
2025-01-25 23:31:58 -0500

    private mongo pw

--- backend/index.js
+++ backend/index.js
@@ -8,19 +8,18 @@ const auth = require("../backend/routes/auth");
 const openAI = require("./routes/openAI");
 
 const mongoose = require("mongoose");
+require("dotenv").config(); // Load environment variables from .env
 
-//add mongoose compass connection logic
-//or add other database connection logic
+// MongoDB connection using .env
+const mongoURI = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@innovateher.2k59h.mongodb.net/`;
 
 mongoose
-  .connect("mongodb+srv://alexli9132:Test@innovateher.2k59h.mongodb.net/")
+  .connect(mongoURI)
   .then(() => console.log("Connected to MongoDB..."))
-  .catch((err) => console.error("Could not connect to MongoDB..."));
+  .catch((err) => console.error("Could not connect to MongoDB...", err));
 
 app.use(cors());
 app.use(express.json());
-
-//add mongoose compass connection logic
 
 app.get("/", (req, res) => {
   res.send("hello world");
@@ -28,7 +27,7 @@ app.get("/", (req, res) => {
 
 app.use("/api/users", users);
 app.use("/api/maps", maps);
-app.use("/api/auth/", auth); //http://localhost:3011/api/auth/user 
+app.use("/api/auth/", auth); // http://localhost:3011/api/auth/user
 app.use("/api/chatbot", openAI);
 
 app.listen(3011, () => {
